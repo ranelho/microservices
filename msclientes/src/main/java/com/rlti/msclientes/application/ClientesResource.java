dccdba263a883d458b41cb282f870bb8203f14ac
ranelho <ranelhol@gmail.com>
2023-02-03 17:20:30 -0300

    implementa gateway

--- msclientes/src/main/java/com/rlti/msclientes/application/ClientesResource.java
+++ msclientes/src/main/java/com/rlti/msclientes/application/ClientesResource.java
@@ -2,6 +2,7 @@ package com.rlti.msclientes.application;
 
 import com.rlti.msclientes.application.representation.ClienteSaveRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -11,12 +12,14 @@ import java.net.URI;
 @RestController
 @RequestMapping("clientes")
 @RequiredArgsConstructor
+@Slf4j
 public class ClientesResource {
 
     private final ClienteService service;
 
     @GetMapping
     public String status(){
+        log.info("Obtendo o status de microservices de clientes");
         return "ok";
     }
 
