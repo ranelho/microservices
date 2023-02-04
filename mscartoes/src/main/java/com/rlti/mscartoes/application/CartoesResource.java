2bbce29de1f098af9ae4cbe5506708c017f2d01b
ranelho <ranelhol@gmail.com>
2023-02-04 09:19:14 -0300

    implementa mscartoes

---
+++ mscartoes/src/main/java/com/rlti/mscartoes/application/CartoesResource.java
@@ -0,0 +1,36 @@
+package com.rlti.mscartoes.application;
+
+import com.rlti.mscartoes.application.representation.CartaoSaveRequest;
+import com.rlti.mscartoes.domain.Cartao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("cartoes")
+@RequiredArgsConstructor
+public class CartoesResource {
+
+    private final CartaoService service;
+
+    @GetMapping
+    public String Status(){
+        return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request){
+        Cartao cartao = request.toModel();
+        service.save(cartao);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda){
+        List<Cartao> list = service.getCartoesRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
+    }
+}
